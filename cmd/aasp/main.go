56fd0845eab861d7d24b2211dbf853cce3f0ed17
Steven Dong <stevendong@microsoft.com>
2023-04-02 22:09:37 -0400

    as

--- cmd/aasp/main.go
+++ cmd/aasp/main.go
@@ -48,10 +48,6 @@ type AzureInformation struct {
 	Identity common.Identity `json:"identity,omitempty"`
 }
 
-var (
-	EncodedUvmInformation common.UvmInformation
-)
-
 var azure_info AzureInformation
 var defaultCertCacheEndpoint = "americas.test.acccache.azure.net"
 
@@ -116,137 +112,6 @@ type KeyProviderProtocolOutput struct {
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	keyprovider.UnimplementedKeyProviderServiceServer
-}
-
-func (s *server) SayHello(ctx context.Context, in *keyprovider.HelloRequest) (*keyprovider.HelloReply, error) {
-	akv := skr.AKV{
-		Endpoint:   "accmhsm.managedhsm.azure.net",
-		APIVersion: "api-version=7.3-preview",
-		//BearerToken: bearerToken,
-	}
-
-	maa := attest.MAA{
-		Endpoint:   "sharedeus2.eus2.test.attest.azure.net",
-		TEEType:    "SevSnpVM",
-		APIVersion: "api-version=2020-10-01",
-	}
-
-	skrKeyBlob := skr.KeyBlob{
-		KID:       "golangversionupdatetest",
-		Authority: maa,
-		AKV:       akv,
-	}
-
-	// MHSM has limit on the request size. We do not pass the EncodedSecurityPolicy here so
-	// it is not presented as fine-grained init-time claims in the MAA token, which would
-	// introduce larger MAA tokens that MHSM would accept
-	//keyBytes, err := skr.SecureKeyRelease("", azure_info.CertCache, azure_info.Identity, skrKeyBlob)
-	keyBytes, err := skr.SecureKeyRelease1(azure_info.CertCache, azure_info.Identity, skrKeyBlob, EncodedUvmInformation)
-
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "SKR failed: %v", err)
-	}
-
-	jwkJSONBytes, err := json.Marshal(keyBytes)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "SKR failed: %v", err)
-	}
-
-	key, err := x509.ParsePKCS8PrivateKey(jwkJSONBytes)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Released key is invalid: %v", err)
-	}
-
-	x, _ := base64.StdEncoding.DecodeString("Id0Q1zXKiOpf3VwvWsxIc4jOr/NHfK5/MerMoz62NN3kFN6quGqSGtzMQVDC+Bqr/g+nMFMl+9V2hCv6n5U15L4c9rlh6OEEqic81y5SDBAzBT1gKa/VZTxZJVZOhOzKSPLqLJzUDVIehuK1+j38MfpG4EfYeG5+17TEwoP/0AKxIp6HXnF3pfWc6BoTrqrCR/eD5nDQKpnuPPhRwJpz/Xwy7hIigQ1/D74U2CUoyornf0hoV3+YdjB2+jKz/V0Od1xnzxl1Okvyuw7Phil/mfsL+oFOc+rfaubjVkNMfhmf5Ju8yFk016PI0T71r7N0GC92YGgD6tKQ1IEqbAuDMjG2RCD1S50KfjeYgCvSIo2QKja8tqyof6Am20XIB/zUr0gNz62f+8zEGCEkqja9hHpN8ZlsXWGAFSd35tHZxlG1WkpRJ7T5OZ8lkt7FUOG+sV8NrbCcbSYrFfYVPiQtPss7T++Z8Hoyx7za+9mD+OpuLBc0Mz77qegBrDrfVVB0")
-
-	var plaintext []byte
-	if privkey, ok := key.(*rsa.PrivateKey); ok {
-		plaintext, err = rsa.DecryptOAEP(sha256.New(), rand.Reader, privkey, x, nil)
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "Unwrapping failed: %v", err)
-		}
-	} else {
-		return nil, status.Errorf(codes.Internal, "Released key is not a RSA private key: %v", err)
-	}
-
-	protocolBytes, _ := json.Marshal(KeyProviderProtocolOutput{
-		KeyUnwrapResults: KeyUnwrapResults{OptsData: plaintext},
-	})
-
-	// return &keyprovider.KeyProviderKeyWrapProtocolOutput{
-	// 	KeyProviderKeyWrapProtocolOutput: protocolBytes,
-	// }, nil
-
-	return &keyprovider.HelloReply{Message: "Hello " + in.GetName() + " " + string(protocolBytes)}, nil
-}
-
-func Test() {
-	akv := skr.AKV{
-		Endpoint:   "accmhsm.managedhsm.azure.net",
-		APIVersion: "api-version=7.3-preview",
-		//BearerToken: bearerToken,
-	}
-
-	maa := attest.MAA{
-		Endpoint:   "sharedeus2.eus2.test.attest.azure.net",
-		TEEType:    "SevSnpVM",
-		APIVersion: "api-version=2020-10-01",
-	}
-
-	skrKeyBlob := skr.KeyBlob{
-		KID:       "golangversionupdatetest",
-		Authority: maa,
-		AKV:       akv,
-	}
-
-	// MHSM has limit on the request size. We do not pass the EncodedSecurityPolicy here so
-	// it is not presented as fine-grained init-time claims in the MAA token, which would
-	// introduce larger MAA tokens that MHSM would accept
-	//keyBytes, err := skr.SecureKeyRelease("", azure_info.CertCache, azure_info.Identity, skrKeyBlob)
-	fmt.Println("about to enter secure key release func")
-	keyBytes, err := skr.SecureKeyRelease1(azure_info.CertCache, azure_info.Identity, skrKeyBlob, EncodedUvmInformation)
-
-	if err != nil {
-		fmt.Printf("SKR failed: %v\n", err)
-	} else {
-		fmt.Println("SKR ok")
-	}
-
-	jwkJSONBytes, err := json.Marshal(keyBytes)
-	if err != nil {
-		fmt.Printf("SKR marshall failed: %v\n", err)
-	} else {
-		fmt.Println("SKR marshall ok")
-	}
-
-	key, err := x509.ParsePKCS8PrivateKey(jwkJSONBytes)
-	if err != nil {
-		fmt.Printf("Released key is invalid: %v\n", err)
-	} else {
-		fmt.Println("Released key is ok")
-	}
-
-	fmt.Println("Released key is ok")
-
-	x, _ := base64.StdEncoding.DecodeString("Id0Q1zXKiOpf3VwvWsxIc4jOr/NHfK5/MerMoz62NN3kFN6quGqSGtzMQVDC+Bqr/g+nMFMl+9V2hCv6n5U15L4c9rlh6OEEqic81y5SDBAzBT1gKa/VZTxZJVZOhOzKSPLqLJzUDVIehuK1+j38MfpG4EfYeG5+17TEwoP/0AKxIp6HXnF3pfWc6BoTrqrCR/eD5nDQKpnuPPhRwJpz/Xwy7hIigQ1/D74U2CUoyornf0hoV3+YdjB2+jKz/V0Od1xnzxl1Okvyuw7Phil/mfsL+oFOc+rfaubjVkNMfhmf5Ju8yFk016PI0T71r7N0GC92YGgD6tKQ1IEqbAuDMjG2RCD1S50KfjeYgCvSIo2QKja8tqyof6Am20XIB/zUr0gNz62f+8zEGCEkqja9hHpN8ZlsXWGAFSd35tHZxlG1WkpRJ7T5OZ8lkt7FUOG+sV8NrbCcbSYrFfYVPiQtPss7T++Z8Hoyx7za+9mD+OpuLBc0Mz77qegBrDrfVVB0")
-
-	var plaintext []byte
-	if privkey, ok := key.(*rsa.PrivateKey); ok {
-		plaintext, err = rsa.DecryptOAEP(sha256.New(), rand.Reader, privkey, x, nil)
-		if err != nil {
-			fmt.Printf("Unwrapping failed: %v\n", err)
-		}
-		fmt.Println("Unwrapping ok.")
-	} else {
-		fmt.Printf("Released key is not a RSA private key: %v", err)
-	}
-
-	protocolBytes, _ := json.Marshal(KeyProviderProtocolOutput{
-		KeyUnwrapResults: KeyUnwrapResults{OptsData: plaintext},
-	})
-
-	fmt.Println("released protocol bytes: ", protocolBytes)
-
 }
 
 // Convert federated token stored in a file into access token for a resource
@@ -414,7 +279,7 @@ func (s *server) UnWrapKey(c context.Context, grpcInput *keyprovider.KeyProvider
 	// 		return nil, status.Errorf(codes.Internal, "Failed to obtain access token to MHSM: %v", err)
 	// 	}
 	// }
-	mhsm := skr.AKV{
+	mhsm := skr.MHSM{
 		Endpoint:   annotation.KmsEndpoint,
 		APIVersion: "api-version=7.3-preview",
 	}
@@ -428,7 +293,7 @@ func (s *server) UnWrapKey(c context.Context, grpcInput *keyprovider.KeyProvider
 	skrKeyBlob := skr.KeyBlob{
 		KID:       annotation.Kid,
 		Authority: maa,
-		AKV:       mhsm,
+		MHSM:      mhsm,
 	}
 
 	azuredata, err := json.Marshal(azure_info)
@@ -443,18 +308,12 @@ func (s *server) UnWrapKey(c context.Context, grpcInput *keyprovider.KeyProvider
 	}
 	fmt.Printf("skrKeyBlob %s\n", string(skrdata))
 
-	uvmin, err := json.Marshal(EncodedUvmInformation)
-	if err != nil {
-		fmt.Println("errdsuauhdksa2")
-	}
-	fmt.Printf("skrKeyBlob %s\n", string(uvmin))
-
 	// MHSM has limit on the request size. We do not pass the EncodedSecurityPolicy here so
 	// it is not presented as fine-grained init-time claims in the MAA token, which would
 	// introduce larger MAA tokens that MHSM would accept
 	//keyBytes, err := skr.SecureKeyRelease("", azure_info.CertCache, azure_info.Identity, skrKeyBlob)
 	//keyBytes, err := skr.SecureKeyReleaseOld(azure_info.CertCache, azure_info.Identity, skrKeyBlob, EncodedUvmInformation)
-	keyBytes, err := skr.SecureKeyReleaseOld("", azure_info.CertCache, azure_info.Identity, skrKeyBlob)
+	keyBytes, err := skr.SecureKeyRelease("", azure_info.CertCache, azure_info.Identity, skrKeyBlob)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "SKR failed: %v", err)
 	} else {
@@ -721,18 +580,10 @@ func main() {
 	if azure_info.Identity.ClientId == "" {
 		log.Printf("Warning: Env AZURE_CLIENT_ID is not set")
 	}
-
-	EncodedUvmInformation, _ = common.GetUvmInfomation()
-	fmt.Println("EncodedSecurityPolicy")
-	fmt.Println(EncodedUvmInformation.EncodedSecurityPolicy)
-	fmt.Println("EncodedUvmReferenceInfo")
-	fmt.Println(EncodedUvmInformation.EncodedUvmReferenceInfo)
-
 	s := grpc.NewServer()
 	keyprovider.RegisterKeyProviderServiceServer(s, &server{})
 	reflection.Register(s)
-	fmt.Println("about to enter Test func")
-	Test()
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to start GRPC server: %v", err)
